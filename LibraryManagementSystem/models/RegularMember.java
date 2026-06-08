d6fdee919e98ce4b65c558342d137856af5a8080
Duy-lion206 <anhduy2024cb@gmail.com>
2026-06-09 00:06:01 +0700

    Extend RegularMember class from Member class

--- LibraryManagementSystem/models/RegularMember.java
+++ LibraryManagementSystem/models/RegularMember.java
@@ -1,5 +1,21 @@
 package models;
 
-public class RegularMember {
+public class RegularMember extends Member{
+    public RegularMember() {
+        super();
+    }
+    public RegularMember(String memberId, String name, int phone) {
+        super(memberId, name, phone);
+    }
+    @Override
+    public double calculateFine(int overdueDays) {
+        // 5k/day
+        return overdueDays * 5000;
+    }
 
+    @Override
+    public String toString() {
+        return super.toString()+ "Type:Regular";
+    }
+    
 }
