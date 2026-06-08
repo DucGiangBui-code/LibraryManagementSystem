ef5762b66c6dd5b554b221d081501f3f116d0a10
Duy-lion206 <anhduy2024cb@gmail.com>
2026-06-09 00:05:03 +0700

    Extend PremiumMember from Member and implement fine logic

--- LibraryManagementSystem/models/PremiumMember.java
+++ LibraryManagementSystem/models/PremiumMember.java
@@ -1,5 +1,24 @@
 package models;
 
-public class PremiumMember {
+public class PremiumMember extends Member{
+    public PremiumMember() {
+        super();
+    }
+    public PremiumMember(String memberId, String name, int phone) {
+        super(memberId, name, phone);
+    }
+    @Override
+    public double calculateFine(int overdueDays) {
+        // free with 3 first day, day 4 5k/day
+        if (overdueDays <= 3) {
+        return 0;
+        }else 
+            return (overdueDays-3)*5000;
+    }
 
+    @Override
+    public String toString() {
+        return super.toString()+ "Type:Premium";
+    }
+    
 }
