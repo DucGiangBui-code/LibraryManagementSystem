2d09b70fee21e9b558e90005068fea3250a392f3
Duy-lion206 <anhduy2024cb@gmail.com>
2026-06-09 00:03:46 +0700

    Add Member class with member details and fine calculation

--- LibraryManagementSystem/models/Member.java
+++ LibraryManagementSystem/models/Member.java
@@ -1,5 +1,52 @@
 package models;
 
-public class Member {
 
+
+public class Member {
+    private String memberId;
+    private String name;
+    private int phone;
+    //calculateFine
+    public double calculateFine(int overdueDays) {
+       return 0;
+    }
+
+    public Member() {
+    }
+
+    public Member(String memberId, String name, int phone) {
+        this.memberId = memberId;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" + "memberId=" + memberId + ", name=" + name + ", phone=" + phone + '}';
+    }
 }
+
